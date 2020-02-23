7e6f4f94300cbd5da057b6ced0ec4c18287d50d1
cesar galindo <csgalindos@hotmail.com>
2020-02-22 19:29:58 -0500

    [create] routes first version

---
+++ src/utils/resolveRoutes.js
@@ -0,0 +1,10 @@
+const resolveRoute = (route) => {
+  if(route.length <=3 ){
+    let validRoute = route === '/'  ? route : '/:id';
+    return validRoute;
+  } else{
+      return `/${route}`
+  }
+}
+
+export default resolveRoute;
